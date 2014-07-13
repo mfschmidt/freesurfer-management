c822172ff5d8c53c41717df5163f018c2b4ffb88
Mike Schmidt <mschmidt@umc.edu>
2014-07-13 12:06:31 -0500

    If the png image already exists, don't bother making a new one. The temp folder is now created in ~, rather than cwd. Output to stdout is more concise.

--- gen_aseg_montage.sh
+++ gen_aseg_montage.sh
@@ -4,9 +4,13 @@
 
 if [ $# -lt "1" ]; then
  echo ""
- echo "USAGE: ${0} <Subject ID>"
+ echo "USAGE: ${0} <Subject ID> [output_directory]"
  echo ""
  echo "Note: SUBJECTS_DIR must be set to directory of Freesurfer data"
+ echo ""
+ echo " If output-directory is specified, the final image will be saved there."
+ echo " otherwise, it will be saved into the subjects original freesurfer"
+ echo " output directory."
  exit;
 fi
 
@@ -18,13 +22,33 @@ if [ ! -d ${SUBJECTS_DIR}/${1} ]; then
 	exit 1
 fi
 
+# Make sure our output directory exists
+OUTDIR=""
+if [ $# -eq 2 ]; then
+	if [ -d ${2} ]; then
+		OUTDIR=${2}
+	else
+		echo "The output directory, ${2}, does not exist."
+		echo "Please create it or try a different output directory."
+		exit 1
+	fi
+else
+	OUTDIR=${SUBJECTS_DIR}/${1}
+fi
+
+# And there's no point overwriting good images
+if [ -f ${OUTDIR}/${1}.png ]; then
+	echo "     ${1}.png exists, skip it.";
+	exit 0
+fi
+
 # make sure our script is ready
 SCT=tkm_gensnapshots.tcl
 TCL=~/bin/${SCT}
 if [ ! -f ${TCL} ]; then
 	TCL=$(find . ~ -name ${SCT} -nowarn 2>/dev/null)
 	if [ ${#TCL} -eq 0 ]; then
-		echo "Can't find ${SCT} file, quitting in desperation."
+		echo "Cannot find ${SCT} file, quitting in desperation."
 		exit 1
 	fi
 fi
@@ -36,6 +60,23 @@ if [ ${#MAG} -eq 0 ]; then
 	exit 1
 fi
 
+# make sure tkmedit is in the path or otherwise available
+TKE=$(which tkmedit)
+if [ ${#TKE} -eq 0 ]; then
+	if [ ${#FREESURFER_HOME} -eq 0 ]; then
+		echo "This script requires tkmedit, usually found in the freesurfer directory."
+		echo "Please add it to your path or set \$FREESURFER_HOME."
+		exit 1
+	else
+		if [ -f ${FREESURFER_HOME}/bin/tkmedit ]; then
+			TKE=${FREESURFER_HOME}/bin/tkmedit
+		else
+			echo "This script requires tkmedit, usually found in the freesurfer directory."
+			echo "Please add it to your path or set \$FREESURFER_HOME."
+		fi
+	fi
+fi
+
 # debugging output for double-checking our dependencies
 #echo "Found subject ${1} at ${SUBJECTS_DIR}/${1},"
 #echo "      script at ${TCL},"
@@ -43,12 +84,15 @@ fi
 #echo "Ready to go."
 
 #Set up a working directory (must match in TCL script)
-mkdir tmp_tkm_tifs
+if [ ! -d ~/tmp_tkm_tifs ]; then
+	mkdir ~/tmp_tkm_tifs;
+fi
 OLDPWD=$(pwd)
-cd tmp_tkm_tifs
+cd ~/tmp_tkm_tifs
 
 # generate the slices and individual images
-tkmedit ${1} brainmask.mgz -aseg -tcl $TCL
+${TKE} ${1} brainmask.mgz -aseg -tcl $TCL
+if [ $? -gt 0 ]; then exit 1; fi;
 
 # crop them all to avoid wasted margins
 for f in $(ls -1f [012]-*.tif); do
@@ -64,10 +108,10 @@ for ori in 0 1 2; do
 	done
 done
 #echo "montaging ${IMGS}"
-montage ${IMGS} -tile 4x3 -geometry 320x320+1+1 ${SUBJECTS_DIR}/${1}/${1}.png
+montage ${IMGS} -tile 4x3 -geometry 320x320+1+1 ${OUTDIR}/${1}.png
 
 # clean up our mess
 cd ${OLDPWD}
-rm -rf tmp_tkm_tifs
+rm -rf ~/tmp_tkm_tifs
 
 
