eba8944867aa507ac063b82adac8897d20d5a616
mfschmidt <mikeschmidt@schmidtgracen.com>
2017-02-16 09:19:20 -0500

    redirect ls error output to /dev/null

--- liaison/do_push.sh
+++ liaison/do_push.sh
@@ -10,11 +10,11 @@ LOC_INBOX=/mri/queue
 
 # First, count how many waiting images are already on the supercomputer.
 # Using "cluster" depends on an entry in /home/$USER/.ssh/config defining it.
-REM_ACTUAL=$(ssh cluster "ls -1 $REM_INBOX/*.tgz | wc -l")
+REM_ACTUAL=$(ssh cluster "2>/dev/null ls -1 $REM_INBOX/*.tgz | wc -l")
 echo "Found ${REM_ACTUAL} on the cluster."
 
 # Next, count how many waiting images are yet to be uploaded.
-LOC_ACTUAL=$(ls -1 $LOC_INBOX/*.tgz | wc -l)
+LOC_ACTUAL=$(2>/dev/null ls -1 $LOC_INBOX/*.tgz | wc -l)
 echo "Found ${LOC_ACTUAL} locally."
 
 if [[ "$LOC_ACTUAL" -gt "0" ]] && [[ "$REM_ACTUAL" -lt "$REM_LIMIT" ]]
@@ -34,6 +34,8 @@ then
 		echo "-----"
 		echo $UNPACK_REP
 		echo "-----"
+		#Everything in the queue is a copy, not an original!! Removing from queue by deletion.
+		rm ${f}
 		cd -
 		COUNTER=$(($COUNTER+1))
 	done
