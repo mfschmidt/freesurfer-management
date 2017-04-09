7bba1e3b694ed7d6ee88d249cc7800b0b9c815cc
mfschmidt <mikeschmidt@schmidtgracen.com>
2017-04-09 11:49:49 -0400

    Completed redundant id and data selection components

--- compare_segs_for_subject.sh
+++ compare_segs_for_subject.sh
@@ -2,37 +2,44 @@
 
 # compare_segs_for_subject.sh
 #
-# This script will generate masks for the subject specified and compare
+# This script will outsource mask generation for the subject specified and compare
 # those masks to all other tracings or segmentations of this subject.
+VERBOSE=1
 
 # First, figure out whether we're asked to deal with a particular tracing
 # or a folder full of them.
 INTYPE=""
+INDIR=""
+INITIALS=""
 if [ -f "$1" ]; then
 	INTYPE="FILE"
+	INDIR="${1%/*}"
 	BASE="${1##*/}"
-	SID="${BASE%%.*}"
-	if [ *"TrueLeft"* == "$BASE" ]; then
-		TL="${1}"
-		TR="${1/Left/Right/}"
-		TI="${1/TrueLeft/COR/}"
-		TI="${TC/xml/img/}"
-		TH="${TI/img/hdr/}"
+	#SID="${BASE%%.*}"
+	IFS=. read SID stuff1 INITIALS stuff2 <<<"${BASE}"
 elif [ -d "$1" ]; then
 	INTYPE="DIR"
-	SID="${1##*/}"
+	INDIR="${1%/}"
+	SID="${INDIR##*/}"
 else
-	echo "$1 is neither a file nor a directory. Nothing I can do."
-	exit 1
+	echo "$1 is neither a file nor a directory. I assume it is the subject ID."
+	SID="$1"
 fi
 
+# Second, determine where to put our masks and results
 OUTDIR=/mri/gmbi.rochester.comparisons/$SID
-
-echo "In     : $1"
-echo "SID    : $SID"
-echo "Out to : $OUTDIR"
-exit 0
-
+if [ "$2" != "" ]; then
+	if [ -d "$2" ]; then
+		OUTDIR="$2"
+	else
+		2>/dev/null mkdir "$2"
+		if [ -d "$2" ]; then
+			OUTDIR="$2"
+		else
+			echo "$2 does not exist and I cannot create it. Using default."
+		fi
+	fi
+fi
 if [ ! -d $OUTDIR ]; then
 	echo "making new directory, $OUTDIR"
 	mkdir $OUTDIR
@@ -41,40 +48,49 @@ if [ ! -d $OUTDIR ]; then
 		exit 1
 	fi
 fi
-echo "Putting all comparison files in $OUTDIR"
+
+if [ $VERBOSE ]; then
+	echo "In      : $1"
+	echo "SID     : $SID"
+	echo "INDIR   : $INDIR"
+	echo "Initials: $INITIALS"
+	echo "Out to  : $OUTDIR"
+	#exit 0
+fi
+
 
 F_LOCI="/mri/gmbi.rochester.fs600/ \
-       /mri/gmbi.rochester.fs530/ \
-       /mri/gmbi.rochester.fs520/ \
-       /mri/gmbi.jackson.fs600/ \
-       /mri/gmbi.jackson.fs530/ \
-       /mri/gmbi.jackson.fs520/"
+        /mri/gmbi.rochester.fs530/ \
+        /mri/gmbi.rochester.fs520/ \
+        /mri/gmbi.jackson.fs600/ \
+        /mri/gmbi.jackson.fs530/ \
+        /mri/gmbi.jackson.fs520/"
 T_LOCI="/mri/gmbi.rochester.trace/ \
-       /mri/gmbi.jackson.trace/"
+        /mri/gmbi.jackson.trace/"
 O_LOCI="/mri/gmbi.rochester.original/ \
-       /mri/gmbi.jackson.original/"
+        /mri/gmbi.jackson.original/"
 
-# We want to compare all freesurfer segmentations and tracings with each other.
-# 1. What freesurfer segmentations do we have?
-F_CANDIDATES=$(2>/dev/null find $F_LOCI -type d -name $1)
-for C in $F_CANDIDATES; do
-	if [ "$(dir_contents.sh $C)" == "freesurfer" ]; then
-		echo "Got FreeSurfer segmentation at $C"
-		gen_masks_from_freesurfer.sh $C $OUTDIR
-	fi
-done
-
-# 2. What manual tracings do we have?
-T_CANDIDATES=$(2>/dev/null find $T_LOCI -type d -name $1)
+# We want to compare the specified freesurfer tracing(s) with all others.
+# 1. What complete manual tracings do we have?
+T_CANDIDATES=$(2>/dev/null find $T_LOCI -type d -name $SID)
 for C in $T_CANDIDATES; do
 	if [ "$(dir_contents.sh $C)" == "mipav" ]; then
 		echo "Got manual tracings at $C"
-		gen_masks_from_xmls.sh $C $OUTDIR
+		#gen_masks_from_xmls.sh $C $OUTDIR
+	fi
+done
+
+# 2. What freesurfer segmentations do we have?
+F_CANDIDATES=$(2>/dev/null find $F_LOCI -type d -name $SID)
+for C in $F_CANDIDATES; do
+	if [ "$(dir_contents.sh $C)" == "freesurfer" ]; then
+		echo "Got FreeSurfer segmentation at $C"
+		#gen_masks_from_freesurfer.sh $C $OUTDIR
 	fi
 done
 
 # 3. And do we have the original images?
-O_CANDIDATES=$(2>/dev/null find $O_LOCI -type d -name $1)
+O_CANDIDATES=$(2>/dev/null find $O_LOCI -type d -name $SID)
 for C in $O_CANDIDATES; do
 	if [ "$(dir_contents.sh $C)" == "analyze" ]; then
 		echo "Got original images at $C"
